4ae8944afb1aab357e41f0b3efd9800fb5d94864
Jose luis <jyana22@gmail.com>
2025-05-13 10:20:56 -0500

    fix: remove cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
-import cloudflare from '@astrojs/cloudflare';
+
+import vercel from "@astrojs/vercel";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(
   )],
-  output: "server",
-  adapter: cloudflare(),
+
   vite: {
     resolve: {
       // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
@@ -20,5 +20,6 @@ export default defineConfig({
       },
     },
   },
-  
+
+  adapter: vercel(),
 });
