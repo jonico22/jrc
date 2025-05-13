54d7e6dd4b969eaf495654578d5f173a05e47ae2
Jose luis <jyana22@gmail.com>
2025-05-12 23:39:14 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,15 +9,8 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [react(
   )],
-  adapter: cloudflare(),
-  vite: {
-    resolve: {
-      // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-      // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-      alias: import.meta.env.PROD && {
-        "react-dom/server": "react-dom/server.edge",
-      },
-    },
-  },
+  adapter: cloudflare(
+    
+  ),
   
 });
